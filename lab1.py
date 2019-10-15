b18a2b9b81004458c709fab345b012fb8504e299
Lexi <adh9570@rit.edu>
2019-10-15 15:42:43 -0400

    initial commit

---
+++ lab1.py
@@ -0,0 +1,19 @@
+import sys
+
+OPEN_LAND_COLOR = '#F89412'
+ROUGH_MEADOW_COLOR = '#FFC000'
+EASY_MOVE_FOREST_COLOR = '#FFFFFF'
+SLOW_RUN_FOREST_COLOR = '#02D03C'
+WALK_FOREST_COLOR = '#028828'
+IMPASS_VEG_COLOR = '#054918'
+WATER_COLOR = '#0000FF'
+ROAD_COLOR = '#473303'
+FOOTPATH_COLOR = '#000000'
+OOB_COLOR = '#CD0065'
+
+if __name__ == "__main__":
+    terrain-image = argv[1]
+    elevation-file = argv[2]
+    path-file = argv[3]
+    season = argv[4]
+    output-image-filename = argv[5] 
