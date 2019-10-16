db8d2249eb5e5e112e37a51e7f60e89c2e17e7cc
Lexi Halbur <jajejdjaje@gmail.com>
2019-10-16 09:50:27 -0400

    moved node class to top

--- lab1.py
+++ lab1.py
@@ -1,6 +1,28 @@
 import sys
 from PIL import Image
 
+class Node:
+    def __init__(self, g, x, y, target):
+        self.x = x
+        self.y = y
+        self.g = g
+        self.h = self.calculateH(target)
+        self.f = g + self.h
+    
+    def getF(self):
+        return self.f
+
+    # for start node
+    def setF(self, f):
+        self.f = f
+
+    def calculateH(self, target):
+        x = abs(self.x - target[0])
+        y = abs(self.y - target[1])
+        self.h = (x**2 + y**2)    # pythagorean theorem
+        return self.h
+
+
 OPEN_LAND_COLOR = (248, 148, 18, 255)
 ROUGH_MEADOW_COLOR = (255, 192, 0, 255)
 EASY_MOVE_FOREST_COLOR = (255, 255, 255, 255)
@@ -111,25 +133,3 @@ if __name__ == "__main__":
     target = getLoc(path_file_name)
     
     search(terrain_pixel_map, elevation_file_name, path_file_name, output_image_filename, location, target)
-
-
-class Node:
-    def __init__(self, g, x, y, target):
-        self.x = x
-        self.y = y
-        self.g = g
-        self.h = self.calculateH(target)
-        self.f = g + self.h
-    
-    def getF(self):
-        return self.f
-
-    # for start node
-    def setF(self, f):
-        self.f = f
-
-    def calculateH(self, target):
-        x = abs(self.x - target[0])
-        y = abs(self.y - target[1])
-        self.h = (x**2 + y**2)    # pythagorean theorem
-        return self.h
