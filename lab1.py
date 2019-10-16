452575a2482478a740f1cc0afabaf0ff09890975
Lexi Halbur <jajejdjaje@gmail.com>
2019-10-16 11:24:55 -0400

    basic a star finished, beginning elevatin consideration

--- lab1.py
+++ lab1.py
@@ -114,7 +114,9 @@ def trimElev(elevation_map_name):
         lines = elevation_file.readlines()
         print(len(lines))
         for line in lines:
+            print(len(line))
             for word in line.split():
+                print(word)
                 if index > len(line) - 6:  # 6 because we want to leave off the last 5, so indexes len(lines) - 6 through len(lines) - 1
                     print("in the last five")
                     continue
